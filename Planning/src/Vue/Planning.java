1aaf6910ac4412c2919d019aab0f58b919b84ae5
XavierAntoine <45229657+XavierAntoine@users.noreply.github.com>
2020-05-27 15:45:13 +0200

    fix RechercherSeanceSemaine

--- Planning/src/Vue/Planning.java
+++ Planning/src/Vue/Planning.java
@@ -13,12 +13,28 @@ public class Planning {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
        //FenetreConnexion test = new FenetreConnexion();
        //test.constructPanel();
+        int i=0;
        
        RechercherSeanceSemaine testSeance = new RechercherSeanceSemaine();
-       ArrayList<String> result = testSeance.SeanceSemaine("abdoulnasir@edu.ece.fr",22);
+       ArrayList<ArrayList<String>> result = testSeance.SeanceSemaine("abdoulnasir@edu.ece.fr",22);
        
-        //for (String result1 : result) {
-          //  System.out.println(result1);
-        //}
+       for(ArrayList<String> iterator : result){
+            i+=1;
+            System.out.println("Cours n°"+i);
+            System.out.println("ID de la séance : " + iterator.get(0));
+            System.out.println("Semaine n°" + iterator.get(1));
+            System.out.println("Jour de la séance : " + iterator.get(2));
+            System.out.println("Heure de début : " + iterator.get(3));
+            System.out.println("Heure de fin : " + iterator.get(4));
+            System.out.println("Matière : " + iterator.get(5));
+            System.out.println("Type de cours : " + iterator.get(6));
+            System.out.println("Professeur : " + iterator.get(7).toUpperCase());
+            System.out.println("Salle : " + iterator.get(8));
+            System.out.println("Site : " + iterator.get(9));
+            for(int k=10;k<iterator.size();k++){
+                System.out.println("Groupe : " + iterator.get(k));
+            }
+            System.out.println("\n-----------------\n");
+        }
     }   
 }
