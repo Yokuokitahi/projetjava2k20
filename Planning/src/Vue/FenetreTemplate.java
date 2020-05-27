4541bf27e87c2d1fccc78c6c51e8ad8f58feaed2
Yokuokitahi <47557670+Yokuokitahi@users.noreply.github.com>
2020-05-27 16:03:00 +0200

    planning edt

--- Planning/src/Vue/FenetreTemplate.java
+++ Planning/src/Vue/FenetreTemplate.java
@@ -1,20 +1,23 @@
 package Vue;
 
+import java.awt.event.*;
 import javax.swing.*;
+
 
 /**
  *
  * @author Xavier Antoine
  */
 public class FenetreTemplate extends JFrame {
-    
-    protected final JFrame fenetre=new JFrame();
+   
+    protected final JFrame fenetre = new JFrame();
     
     public FenetreTemplate(){
         fenetre.setTitle("Planning");
         fenetre.setSize(900,900);
         fenetre.setLocationRelativeTo(null);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         fenetre.setVisible(true);
     }
         
--- Planning/src/Vue/Planning.java
+++ Planning/src/Vue/Planning.java
@@ -1,24 +1,16 @@
 package Vue;
 
-import Controleur.RechercherSeanceSemaine;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 /**
  *
  * @author xavan
  */
 public class Planning {
-        
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-       //FenetreConnexion test = new FenetreConnexion();
-       //test.constructPanel();
-       
-       RechercherSeanceSemaine testSeance = new RechercherSeanceSemaine();
-       ArrayList<String> result = testSeance.SeanceSemaine("abdoulnasir@edu.ece.fr",22);
-       
-        //for (String result1 : result) {
-          //  System.out.println(result1);
-        //}
-    }   
+
+    public static void main(String[] args) throws SQLException {
+       FenetreConnexion test = new FenetreConnexion();
+       test.constructPanel();
+    }
+    
 }
