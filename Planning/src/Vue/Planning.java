efcb522beedb6e46d4e94051dff76c18dda63591
Yokuokitahi <47557670+Yokuokitahi@users.noreply.github.com>
2020-06-01 19:00:56 +0200

    cght

--- Planning/src/Vue/Planning.java
+++ Planning/src/Vue/Planning.java
@@ -12,12 +12,12 @@ public class Planning {
         
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         
-       //FenetreConnexion test = new FenetreConnexion();
-       //test.constructPanel();
-       AjouterDB yes = new AjouterDB();
+       FenetreConnexion test = new FenetreConnexion();
+       test.constructPanel();
+       /*AjouterDB yes = new AjouterDB();
        ArrayList<String> groupes = new ArrayList<>();
        groupes.add("A");
        groupes.add("B");
-       yes.AjouterSeance("2020-06-06", "10:15", "Analyse de Fourier", "Cours Magistral", "coudray", groupes, "2023", "Amphi A");
+       yes.AjouterSeance("2020-06-06", "10:15", "Analyse de Fourier", "Cours Magistral", "coudray", groupes, "2023", "Amphi A");*/
     }   
 }
