174ac33669f54c5aaa1a6a1fc9adcebb4c65a1ee
XavierAntoine <45229657+XavierAntoine@users.noreply.github.com>
2020-06-05 15:44:31 +0200

    dd

--- Planning/src/Vue/Planning.java
+++ Planning/src/Vue/Planning.java
@@ -1,6 +1,7 @@
 package Vue;
 
 import Controleur.InfosDB;
+import Controleur.RechercherSeance;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -12,12 +13,14 @@ public class Planning {
         
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         
-       FenetreConnexion test = new FenetreConnexion();
-       test.constructPanel();
+       //FenetreConnexion test = new FenetreConnexion();
+       //test.constructPanel();
        /* ArrayList<String> tt;
        RechercherSeance test = new RechercherSeance();
        tt = test.RecapSeance("malo", "Initiation réseau");
        double nbHeures = (tt.size())*1.5;
        System.out.println("Initiation réseau : " + nbHeures+ " heures");*/
+       ArrayList<ArrayList<String>> tes = RechercherSeance.Seance();
+       System.out.println(tes);
     }   
 }
