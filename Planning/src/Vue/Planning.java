783aa35b8a41a9b92b55ebc66abb7ad4f9518a09
XavierAntoine <45229657+XavierAntoine@users.noreply.github.com>
2020-06-05 16:22:42 +0200

    fix affichage liste

--- Planning/src/Vue/Planning.java
+++ Planning/src/Vue/Planning.java
@@ -13,14 +13,12 @@ public class Planning {
         
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         
-       //FenetreConnexion test = new FenetreConnexion();
-       //test.constructPanel();
+       FenetreConnexion test = new FenetreConnexion();
+       test.constructPanel();
        /* ArrayList<String> tt;
        RechercherSeance test = new RechercherSeance();
        tt = test.RecapSeance("malo", "Initiation réseau");
        double nbHeures = (tt.size())*1.5;
        System.out.println("Initiation réseau : " + nbHeures+ " heures");*/
-       ArrayList<ArrayList<String>> tes = RechercherSeance.Seance();
-       System.out.println(tes);
     }   
 }
