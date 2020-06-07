0fb1998e7abe2eb405863290041d7ef070e37f6c
Yokuokitahi <47557670+Yokuokitahi@users.noreply.github.com>
2020-06-07 16:32:19 +0200

    fenetre co

--- Planning/src/Vue/FenetreConnexion.java
+++ Planning/src/Vue/FenetreConnexion.java
@@ -18,52 +18,51 @@ import javax.swing.*;
 
 public class FenetreConnexion extends FenetreTemplate{
     
-    private JTextField login;
-    private JPasswordField password;
-    private JLabel labelLogin, labelPassword, titre;
-    private JButton connectButton;
-    private Container container = null;
+    private final JTextField login = new JTextField();
+    private final JPasswordField password = new JPasswordField();;
+    private final JLabel labelLogin = new JLabel("Nom d'utilisateur:");
+    private final JLabel labelPassword = new JLabel("Mot de passe:");
+    private final JTextPane titre = new JTextPane();
+    private final JButton connectButton = new JButton("Se connecter");
+    private final JPanel container = new JPanel();
     private final Connexion connexion;
-    private int logUser =0;
+    private int logUser = 0;
     
     public FenetreConnexion() throws SQLException, ClassNotFoundException {
         connexion = new Connexion();
-        container = fenetre; 
-    }
-    
-    
-    public void constructPanel(){
-            
+        
+        fenetre.setContentPane(container);
+        fenetre.setSize(901,900);
+        container.setLayout(null);
+        
         Font police  = new Font(Font.SERIF, Font.PLAIN,  28);
         Font police2  = new Font(Font.SERIF, Font.PLAIN,  40);
-        //creation des labels 
-        labelLogin=new JLabel("Nom d'utilisateur:");
-        labelLogin.setFont(police);
-        labelPassword=new JLabel("Mot de passe:");
+        labelLogin.setFont(police); 
         labelPassword.setFont(police);
-        titre = new JLabel("Bienvenue");
+        
+        titre.setText("Bienvenue");
         titre.setFont(police2);
-        // creation des champs de texte
-        password=new JPasswordField();
+        titre.setEditable(false);
+        titre.setBackground(container.getBackground());
         password.setEchoChar('*');
-        login=new JTextField();
         
-         // creation des boutons
-        connectButton=new JButton("Se connecter");
-        
-        //ajout des items sur le buffer
-        container.add(login);
-        container.add(password);
-        container.add(connectButton);
-        container.add(labelLogin);
-        container.add(labelPassword);
-        container.add(titre);
         titre.setBounds(350,150,400,60);
         login.setBounds(400, 360, 300, 50);
         password.setBounds(400, 460, 300, 50);
         connectButton.setBounds(350, 550, 150, 60);
         labelLogin.setBounds(130, 350, 200, 60);
         labelPassword.setBounds(130, 450, 200, 60);
+        
+        container.add(login);
+        container.add(password);
+        container.add(connectButton);
+        container.add(labelLogin);
+        container.add(labelPassword);
+        container.add(titre);
+    }
+    
+    public void constructPanel(){     
+
         connectButton.addActionListener(new ActionListener ()
         {
             @Override
